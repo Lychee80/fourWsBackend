a89aed50e38433a8d3a88ed497b41d831ad7a94f
lwu1822 <lwu1822@gmail.com>
2023-05-14 11:47:10 -0700

    add test user, remove sqlite.db from .gitignore

--- api/user.py
+++ api/user.py
@@ -24,7 +24,7 @@ class UserAPI:
                 return {'message': f'Name is missing, or is less than 2 characters'}, 400
             # validate uid
             uid = body.get('uid')
-            if uid is None or len(uid) < 2:
+            if uid is None or len(uid) < 1:
                 return {'message': f'User ID is missing, or is less than 2 characters'}, 400
             # look for password and dob
             password = body.get('password')
