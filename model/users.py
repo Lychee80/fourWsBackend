be9413ff5e405ed6b24b852ee92df37fe748d130
lwu1822 <lwu1822@gmail.com>
2023-05-14 13:51:36 -0700

    create recommended songs

--- model/users.py
+++ model/users.py
@@ -194,6 +194,94 @@ class User(db.Model):
         return None
 
 
+
+class Song(db.Model):
+    __tablename__ = 'songs'  # table name is plural, class name is singular
+
+    # Define the User schema with "vars" from object
+    id = db.Column(db.Integer, primary_key=True)
+    _uid = db.Column(db.String(255), unique=True, nullable=False)
+    _song = db.Column(db.String(255), unique=False, nullable=False)
+
+    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
+    #posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
+
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, uid, song):
+        self._uid = uid    # variables with self prefix become part of the object, 
+        self._song = song
+
+    
+    # a getter method, extracts email from object
+    @property
+    def uid(self):
+        return self._uid
+    
+    # a setter function, allows name to be updated after initial object creation
+    @uid.setter
+    def uid(self, uid):
+        self._uid = uid
+        
+    # check if uid parameter matches user id in object, return boolean
+    def is_uid(self, uid):
+        return self._uid == uid
+    
+    # a name getter method, extracts name from object
+    @property
+    def song(self):
+        return self._song
+    
+    # a setter function, allows name to be updated after initial object creation
+    @song.setter
+    def song(self, song):
+        self._song = song
+    
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
+    def __str__(self):
+        return json.dumps(self.read())
+
+    # CRUD create/add a new record to the table
+    # returns self or None on error
+    def create(self):
+        try:
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read converts self to dictionary
+    # returns dictionary
+    def read(self):
+        return {
+            "id": self.id,
+            "uid": self.uid,
+            "song": self.song,
+        }
+
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, uid="", song=""):
+        """only updates values with length"""
+        if len(uid) > 0:
+            self.uid = uid
+        if len(song) > 0:
+            self.set_song(song)
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+
 """Database Creation and Testing """
 
 
