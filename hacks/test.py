b482e9b6f8647976e6c8cd6bc5de81e80c39d46c
aidanywu <aidanywu@gmail.com>
2023-05-17 10:00:12 -0700

    noise

--- hacks/test.py
+++ hacks/test.py
@@ -1,21 +1,21 @@
 from sklearn.linear_model import LinearRegression
 import random
+import numpy as np
 
 feature_set = []
 target_set = []
 
 # 200 rows of data
-number_of_rows = 200
+number_of_rows = 10
 random_number_limit = 2000
 
 for i in range(0, number_of_rows):
-    x= random.randint(0, random_number_limit)
-    y= random.randint(0, random_number_limit)
-    z= random.randint(0, random_number_limit)
+    x= random.randint(random_number_limit*-1, random_number_limit)
+    y= random.randint(random_number_limit*-1, random_number_limit)
+    z= random.randint(random_number_limit*-1, random_number_limit)
 
     # linear function
-    function = (10*x)+(2*y)+(3*z)
-
+    function = (10*x)+(2*y)+(3*z) + np.random.normal(scale=1000)
     feature_set.append([x,y,z])
     target_set.append(function)
 
