c01bdef65ed339221dd798ef7e6ea0cc3051364b
aidanywu <aidanywu@gmail.com>
2023-05-22 08:44:18 -0700

    song rec system: cosine sim and matrix fac

---
+++ song_rec/similarity.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.linalg import norm
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
+
+# imported cosine_similarity and linear_kernel keeps giving memory error
+def cossim(A):
+    cosine = np.sum(A*A, axis=1)/(norm(A, axis=1)*norm(A, axis=1))
+    return cosine
+
+# putting movies data on 'movies' dataframe
+# take genre, time signature, instrumentalness, key, explicit, duration, and album into account later
+songs = pd.read_csv('data.csv')
+features = songs[['popularity','danceability','energy','loudness','speechiness','acousticness','valence','tempo']].values.astype(np.float16)
+# features = songs[['popularity']].values
+print(features[:5,:])
+
+similarity_matrix = cosine_similarity(features)
+print(similarity_matrix[:5,:])
