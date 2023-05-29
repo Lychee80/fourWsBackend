b3f542415d9bab242c0aa68e091378ff12cab2c5
lwu1822 <lwu1822@gmail.com>
2023-05-29 14:48:13 -0700

    broken data; don't clone

--- recommender2.py
+++ recommender2.py
@@ -4,48 +4,58 @@ from typing import Tuple, List
 import implicit
 import scipy
 
-from data import loadUserArtist, ArtistRetriever
+from data2 import loadCountrySong, SongRetriever
 
 
 class ImplicitRecommender:
 
-    def __init__(self, artist_retriever, implicit_model):
-        self.artist_retriever = artist_retriever
+    def __init__(self, song_retriever, implicit_model):
+        self.song_retriever = song_retriever
         self.implicit_model = implicit_model
 
-    def fit(self, user_artists_matrix):
+    def fit(self, country_songs_matrix):
         # train the implicit model with the user artists matrix (user_artists.dat)
-        self.implicit_model.fit(user_artists_matrix)
+        self.implicit_model.fit(country_songs_matrix)
 
-    def recommend(self, user_id, user_artists_matrix, recommendNum=10):
+    def recommend(self, country_id, country_songs_matrix, recommendNum=10):
         # get a list of recommendations based on the user id
-        artist_ids, scores = self.implicit_model.recommend(
-            user_id, user_artists_matrix[recommendNum], N=recommendNum
+        song_ids, scores = self.implicit_model.recommend(
+            country_id, country_songs_matrix[recommendNum], N=recommendNum
         )
         # return artists names from artist ids
-        artists = [
-            self.artist_retriever.getArtistNameFromId(artist_id)
-            for artist_id in artist_ids
+        
+        print(song_ids)
+        print(scores)
+        """
+        songs = [
+            self.song_retriever.getSongNameFromId(song_id)
+            for song_id in song_ids
         ]
-        return artists, scores
+        """
+        # return songs, scores
+        return 1, 2
 
 
 if __name__ == "__main__":
 
-    user_artists = loadUserArtist("data/user_artists.dat")
+    country_songs = loadCountrySong("data/test/country_song.dat")
 
-    artist_retriever = ArtistRetriever()
-    artist_retriever.loadArtists("data/artists.dat")
+    song_retriever = SongRetriever()
+    song_retriever.loadSongs("data/test/song.dat")
 
     # use alternating least squares
     implict_model = implicit.als.AlternatingLeastSquares(
         factors=50, iterations=10, regularization=0.01
     )
 
-    recommender = ImplicitRecommender(artist_retriever, implict_model)
+    
+    recommender = ImplicitRecommender(song_retriever, implict_model)
     # train
-    recommender.fit(user_artists)
-    artists, scores = recommender.recommend(2101, user_artists, 5)
-
-    for artist, score in zip(artists, scores):
-        print(f"{artist}: {score}")
+    recommender.fit(country_songs)
+    print(recommender.recommend(1, country_songs, 1))
+    """
+    songs, scores = recommender.recommend(1, country_songs, 1)
+    
+    for song, score in zip(songs, scores):
+        print(f"{song}: {score}")
+    """
